54cbf2b4a2a763b7f27bfff914fef1729332ac8e
Albert <nomadriding@gmail.com>
2024-01-06 22:28:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = [];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const goldText = document.querySelector("#goldText");
+
+// Below is the portion of RPS (Rock Paper Scissors)
+
+let userGuess;
+let play;
+let computerChoice;
